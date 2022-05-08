c268a7d528940b9f3947748ab1778be0c86fe440
miki4920 <miki4920@gmail.com>
2022-05-08 18:59:14 +0100

    Balanced tools

--- src/main/java/com/miki4920/furnacemod/item/ModItems.java
+++ src/main/java/com/miki4920/furnacemod/item/ModItems.java
@@ -14,19 +14,19 @@ public class ModItems {
             () -> new Item(new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static final RegistryObject<Item> OBSIDIAN_INGOT_SWORD = ITEMS.register("obsidian_ingot_sword",
-            () -> new SwordItem(ModTiers.OBSIDIAN_INGOT, 7, 1f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
+            () -> new SwordItem(ModTiers.OBSIDIAN_INGOT, 6, -2.4f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static final RegistryObject<Item> OBSIDIAN_INGOT_PICKAXE = ITEMS.register("obsidian_ingot_pickaxe",
-            () -> new PickaxeItem(ModTiers.OBSIDIAN_INGOT, 1, 1f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
+            () -> new PickaxeItem(ModTiers.OBSIDIAN_INGOT, -1, -2.4f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static final RegistryObject<Item> OBSIDIAN_INGOT_AXE = ITEMS.register("obsidian_ingot_axe",
-            () -> new AxeItem(ModTiers.OBSIDIAN_INGOT, 1, 1f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
+            () -> new AxeItem(ModTiers.OBSIDIAN_INGOT, 6, -3.2f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static final RegistryObject<Item> OBSIDIAN_INGOT_HOE = ITEMS.register("obsidian_ingot_hoe",
-            () -> new HoeItem(ModTiers.OBSIDIAN_INGOT, 1, 1f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
+            () -> new HoeItem(ModTiers.OBSIDIAN_INGOT, -1, -2.4f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static final RegistryObject<Item> OBSIDIAN_INGOT_SHOVEL = ITEMS.register("obsidian_ingot_shovel",
-            () -> new ShovelItem(ModTiers.OBSIDIAN_INGOT, 1, 1f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
+            () -> new ShovelItem(ModTiers.OBSIDIAN_INGOT, -1, -2.4f, new Item.Properties().fireResistant().tab(ModCreativeModeTab.FURNACE_MOD_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
