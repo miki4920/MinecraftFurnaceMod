8afe44754aec2eafe847203db2a378802697e8be
miki4920 <miki4920@gmail.com>
2022-05-08 18:59:29 +0100

    Changed the icon for the creative tab

--- src/main/java/com/miki4920/furnacemod/item/ModCreativeModeTab.java
+++ src/main/java/com/miki4920/furnacemod/item/ModCreativeModeTab.java
@@ -1,5 +1,6 @@
 package com.miki4920.furnacemod.item;
 
+import com.miki4920.furnacemod.block.ModBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -7,7 +8,7 @@ public class ModCreativeModeTab {
     public static final CreativeModeTab FURNACE_MOD_TAB = new CreativeModeTab("furnacetab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.OBSIDIAN_INGOT.get());
+            return new ItemStack(ModBlocks.OBSIDIAN_INGOT_BLOCK.get());
         }
     };
 }
