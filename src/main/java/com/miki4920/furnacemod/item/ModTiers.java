35ec1baa43b95d6fe912f87d4ba99aa389ec0f6d
miki4920 <miki4920@gmail.com>
2022-05-08 16:08:11 +0100

    Added obsidian tools

---
+++ src/main/java/com/miki4920/furnacemod/item/ModTiers.java
@@ -0,0 +1,11 @@
+package com.miki4920.furnacemod.item;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+
+public class ModTiers {
+    public static final ForgeTier OBSIDIAN_INGOT = new ForgeTier(4, 5000, 16f,
+            6f, 30, BlockTags.NEEDS_DIAMOND_TOOL,
+            () -> Ingredient.of(ModItems.OBSIDIAN_INGOT.get()));
+}
